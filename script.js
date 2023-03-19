94d8ed7cf640fe94d5ddb098d4a234f10f6b69fe
NazimS <nazim.shixkerimov@gmail.com>
2023-03-19 11:37:18 +0400

    all sections and including adaptive design fixed and finished

--- script.js
+++ script.js
@@ -22,10 +22,9 @@ function openMenu(evt, eatingTime) {
     let i, tabcontent, tab;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-      // tabcontent[tabcontent.length -1].style.display = "none";
-      
+      tabcontent[i].style.display = "none";  
     }
+    
     tab = document.getElementsByClassName("tab");
     for (i = 0; i < tab.length; i++) {
       tab[i].className = tab[i].className.replace(" active", "");
