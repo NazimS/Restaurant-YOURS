494484c6efad571bfe04e009aaea881c61f51c67
NazimS <nazim.shixkerimov@gmail.com>
2023-03-25 22:55:45 +0400

    finished

--- script.js
+++ script.js
@@ -7,38 +7,34 @@ const navMenu = document.querySelector('.nav-menu')
 let sticky = navMenu.offsetTop;
 
 function fixedNavbar() {
+
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     navMenu.classList.add("sticky");
   } else {
     navMenu.classList.remove("sticky");
   }
+
 }
 
 // navbar sticky end
 
 // burger-menu begin
 
-// $(() => {
-//   $('.openMenu').click(function () {
-//     $('.mainMenu').slideToggle('slow');
-//   });
-//   $('.closeMenu').click(function () {
-//     $('.mainMenu').slideToggle('slow');
-//   });
-// });
-
 $(() => {
+
   $(".openMenu").click(function () {
     $(".mainMenu").animate({   // right to left
       width: "toggle"
     });
 
   });
+
   $(".closeMenu").click(function () {
     $(".mainMenu").animate({  // left to right
       width: "toggle"
     });
   });
+
 });
 
 // burger-menu end
@@ -46,6 +42,7 @@ $(() => {
 //tab menu begin
 
 function openMenu(evt, eatingTime) {
+
   let i, tabcontent, tab;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
@@ -58,6 +55,7 @@ function openMenu(evt, eatingTime) {
   }
   document.getElementById(eatingTime).style.display = "flex";
   evt.currentTarget.className += " active";
+
 }
 
 //tab menu end
